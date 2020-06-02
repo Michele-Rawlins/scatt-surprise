3873db804017c77fafd14ef31ae03a3d4af7202f
Michele Rawlins <shellbop@att.net>
2020-06-01 21:56:39 -0500

    setup complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,70 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './App.scss';
 
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
+
+import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
+
+import Auth from '../components/pages/Auth/Auth';
+import EditScat from '../components/pages/EditScat/EditScat';
+import Home from '../components/pages/Home/Home';
+import NewScat from '../components/pages/NewScat/NewScat';
+import SingleScat from '../components/pages/SingleScat/SingleScat';
+
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">CRAP</button>
+        <MyNavbar />
+        <h1>Scat Surprise</h1>
+        <Auth />
+        <EditScat />
+        <Home />
+        <NewScat />
+        <SingleScat />
       </div>
     );
   }

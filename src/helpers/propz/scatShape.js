99c170a95a800dba39fcb144ecfdb95a273dd5e3
Michele Rawlins <shellbop@att.net>
2020-06-13 10:31:29 -0500

    edit works

--- src/helpers/propz/scatShape.js
+++ src/helpers/propz/scatShape.js
@@ -5,9 +5,9 @@ const scatShape = PropTypes.shape({
   color: PropTypes.string.isRequired,
   shape: PropTypes.string.isRequired,
   size: PropTypes.string.isRequired,
-  temperature: PropTypes.number.isRequired ,
+  temperature: PropTypes.number.isRequired,
   viscosity: PropTypes.string.isRequired,
-  wasFulfilling: PropTypes.bool.isRequired ,
+  wasFulfilling: PropTypes.bool.isRequired,
   location: PropTypes.string.isRequired,
   notes: PropTypes.string.isRequired,
 
